331a79814ef6eea1cbe615e995ccc12d9582394e
Kecskés Gellért <gellertkecskes@gmail.com>
2026-01-16 21:50:43 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,41 @@
+//fetch() = lekéri az apit az url-ről await meg addig nem futtatja tovább a kódot ami nem küld vissza üzenet
+const apiKey ="863242cfb2b1d357е6093d9a4df19a4b";
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + '&appid=${apiKey}');
+
+    if(response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+    } else if(data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png";
+    } else if(data.weather[0].main == "rain") {
+        weatherIcon.src = "images/rain.png"
+    } else if(data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+    } else if(data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png";
+    }
+    document.querySelector(".weather").computedStyleMap.display = "block";
+    document.querySelector(".error").style.display = "none";
+}        
+    }
+    var data = await response.json(); //meg várja az adatot(fetch) és feldolgoza json-ra
+  
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)  + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h"; //innerHTML = ide irjuk hogy az api key-nek a az adati nevet
+
+searchBtn.addEventListener("click",function() {
+    checkWeather(searchBox.value);
+})
+checkWeather(); //azonnal fusson a lekérés a (fuction)
